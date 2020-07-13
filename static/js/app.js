f7276987a42bcaad10911ca6195a8606d5f7107a
Jorge Calderon Noguez <jornej.23@gmail.com>
2020-07-13 16:06:49 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,64 +1,64 @@
-let sampleBellyData
+d3.json("samples.json").then((samples_data) => {
+    let option = d3.select("#selDataset");
+    let data_names = samples_data.names
 
-d3.json("samples.json").then((data) => {
-    sampleBellyData = data;
-
-    let option = d3.select("#selDataset");    
-    sampleBellyData.names.forEach(d => {
-        option
-        .append("option")
-        .property("value", d)
-        .text(d)
+    // Setting up the selection options
+    data_names.forEach(d =>{
+        option.append("option")
+            .property("value", d)
+            .text(d)
     });
+
 });
 
-function optionChanged(sel){
-    // getting data for the information chart and the plots
-    let metadata = sampleBellyData.metadata.filter(d => d.id === parseInt(sel))[0];
-    let samples = sampleBellyData.samples.filter(d => d.id === sel)[0];
-    InfoChart(metadata);
-    Plots(samples);
-}
+function optionChanged(selection){
+    d3.json("samples.json").then((samples_data) => {
+        let sel = d3.select("#sample-metadata"); 
+        let data_names = samples_data.names
+        
+        // getting data for the information chart and the plots
+        let metadata = samples_data.metadata.filter(d => d.id === parseInt(selection))[0];
+        let samples = samples_data.samples.filter(d => d.id === selection)[0];
+        //console.log(metadata)
 
-// Information Chart
-function InfoChart(data){
-    let option = d3.select("#sample-metadata"); 
-    let entries_data = Object.entries(data);
-    option.selectAll("p").remove()
-    entries_data.forEach(d => {
-        option
-        .append("p")
-        .text(`${d[0]}: ${d[1]}`)
+        // Making Information Chart
+        sel.selectAll("p").remove()
+        let entries_data = Object.entries(metadata)
+        //console.log(entries_data)
+        
+        entries_data.forEach(d => {
+            sel
+            .append("p")
+            .text(`${d[0]}: ${d[1]}`)
+        });
+
+        // Plotting Bar plot
+        let traceBar = {
+            x: samples.sample_values,
+            y: samples.otu_ids.sort((a, b) => a - b).map(d => "OTU " + d),
+            text: samples.otu_labels,
+            type: "bar",
+            orientation: "h"
+        };
+    
+        let dataBar = [traceBar]  
+        Plotly.newPlot("bar", dataBar);
+    
+        // Plotting Bubble plot
+        traceBuble = {
+            x: samples.otu_ids.sort((a, b) => a - b),
+            y: samples.sample_values,
+            text: samples.otu_labels,
+            mode: "markers",
+            marker: {
+                size: samples.sample_values, 
+                sizeref: 0.1,
+                sizemode: "area"
+            }
+        };
+    
+        let dataBubble = [traceBuble];
+        Plotly.newPlot("bubble", dataBubble);
+
     });
-}
-
-// Plotting
-function Plots(samples){
-    // Plotting Bar chart
-    let traceBar = {
-        x: samples.sample_values,
-        y: samples.otu_ids.sort((a, b) => a - b).map(d => "OTU " + d),
-        text: samples.otu_labels,
-        type: "bar",
-        orientation: "h"
-    };
-
-    let dataBar = [traceBar]  
-    Plotly.newPlot("bar", dataBar);
-
-    // Plotting Bubble plot
-    traceBuble = {
-        x: samples.otu_ids.sort((a, b) => a - b),
-        y: samples.sample_values,
-        text: samples.otu_labels,
-        mode: "markers",
-        marker: {
-            size: samples.sample_values, 
-            sizeref: 0.1,
-            sizemode: "area"
-        }
-    };
-
-    let dataBubble = [traceBuble];
-    Plotly.newPlot("bubble", dataBubble);
 };
